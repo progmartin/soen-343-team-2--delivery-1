972f9a284a20e89e45e6e624b91d7bd914db80e4
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-10-17 17:39:03 -0400

    Added Temporary Simulation Interfaces
    Added some functions to be used by the GUI.

--- ProjectCode/src/simulation/Simulation.java
+++ ProjectCode/src/simulation/Simulation.java
@@ -5,10 +5,40 @@
  */
 package simulation;
 
+import HouseObjects.Room;
+import java.util.ArrayList;
+
 /**
  *
  * @author DRC
  */
 public class Simulation {
     
+    ArrayList<Room> rooms;
+
+    public Simulation() {
+        rooms = new ArrayList<>();
+    }
+    
+    
+    
+    public String getUserLocation(String user){
+        return "location";
+    }
+    
+    public void setUserLocation(String user, String location){
+        
+    }
+    
+    public ArrayList getDoors(){
+        for (Room r : rooms){
+            //r.getDoors();
+        }
+        return new ArrayList<>();
+    }
+    
+    
+    public ArrayList<Room> getRooms(){
+        return rooms;
+    }
 }
